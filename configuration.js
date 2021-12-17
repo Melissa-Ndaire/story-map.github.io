680bb3045d39b07ec5a41fbe62330ce978099c4f
Melissa-Ndaire <96300329+Melissa-Ndaire@users.noreply.github.com>
2021-12-17 21:43:43 +0300

    Create configuration.js

---
+++ configuration.js
@@ -0,0 +1,22 @@
+<script type="text/javascript">
+      //---------------------------------
+      //     Application configuration
+      //---------------------------------
+      var configOptions = {
+        // Enter an application ID created through the Cascade builder
+        appid: "a22c0c5113e64dd7b9fb6248e4b976c3",
+        // Optionally, to secure Cascade's access, if the story item is private or to use the builder
+        //    configure an ArcGIS OAuth application ID (example: 6gyOg377fLUhUk6f)
+        // When specified, sign-in is mandatory even if your story is public
+        oAuthAppId: "",
+        // Optionally, to use the appid URL parameter, configure authorizedOwners to reference
+        //    members whose stories can be viewed by this storytelling app.
+        // To authorize stories owned by...
+        //    specific members: use ["member"] or ["member1", "member2", ...]
+        //    any ArcGIS member: use ["*"]
+        //    any member of one or more organizations: use ["[orgID]"] or ["[orgID1]", "[orgID2]", ...] (Note the use of brackets in this case, e.g., ["[nzS0F0zdNLvs7nc8]"])
+        //        You can get your orgID by going to My Organization and clicking Find...The most viewed items in the organization.
+        //        Your orgID will be shown in the search box.
+        authorizedOwners: [""]
+      };
+    </script>
